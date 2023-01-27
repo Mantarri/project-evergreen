1988891c42ba2e050ae17250f089709e3966602f
TheMisteryMan <TheMisteryMan@users.noreply.github.com>
2023-01-26 21:48:32 -0800

    Change: Camera and character rotation now use `rotation_degrees` as opposed to `rotation` (radians) for better readability, `direction` can now be access outside of `_physics_process` on `Player`

--- scenes/entities/automatons/player.gd
+++ scenes/entities/automatons/player.gd
@@ -8,6 +8,7 @@ const JUMP_VELOCITY = 7.75
 
 @export
 var mouse_sensitivity: float = 0.075
+var direction: Vector3
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = 17.64
@@ -26,32 +27,32 @@ func _unhandled_input(event):
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		$Camera3D.rotation.x -= deg_to_rad(event.relative.y * mouse_sensitivity)
-		$Camera3D.rotation.x = deg_to_rad(clamp(rad_to_deg($Camera3D.rotation.x), -90.0, 90.0))
+		$Camera3D.rotation_degrees.x -= event.relative.y * mouse_sensitivity
+		$Camera3D.rotation_degrees.x = clamp($Camera3D.rotation_degrees.x, -90.0, 90.0)
 		
-		rotation.y -= deg_to_rad(event.relative.x * mouse_sensitivity)
-		rotation.y = deg_to_rad(wrapf(rad_to_deg(rotation.y), 0.0, 360.0))
+		rotation_degrees.y -= event.relative.x * mouse_sensitivity
+		rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
+
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
+	
 	# Handle Jump.
 	if Input.is_action_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
 	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-#		velocity.x = move_toward(velocity.x, 0, SPEED)
-#		velocity.z = move_toward(velocity.z, 0, SPEED)
-		velocity.x = 0
-		velocity.z = 0
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+#		velocity.x = 0
+#		velocity.z = 0
 	
 	move_and_slide()
