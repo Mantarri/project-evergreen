7b4c962896f16b40b31528c33407f5e2f908164c
Mistery Man <themisteryman2@outlook.com>
2023-03-01 13:45:13 -0800

    Change: Move properties over to `Components`, change camera-related things, add walk sfx

--- scenes/entities/automatons/player.gd
+++ scenes/entities/automatons/player.gd
@@ -2,16 +2,18 @@ extends CharacterBody3D
 
 class_name Entity
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 7.75
+var speed: float = 5.0
+var jump_velocity = 7.0
 
+@onready
+var component_holder: ComponentHolder = $ComponentHolder
 
 @export
 var mouse_sensitivity: float = 0.075
 var direction: Vector3
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: float = 17.64
+@onready
+var camera: Camera3D = $Camera3D
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -27,32 +29,54 @@ func _unhandled_input(event):
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		$Camera3D.rotation_degrees.x -= event.relative.y * mouse_sensitivity
-		$Camera3D.rotation_degrees.x = clamp($Camera3D.rotation_degrees.x, -90.0, 90.0)
+		camera.rotation_degrees.x -= event.relative.y * mouse_sensitivity
+		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90.0, 90.0)
 		
 		rotation_degrees.y -= event.relative.x * mouse_sensitivity
 		rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
 
-
+func _process(_delta: float) -> void:
+	if direction:
+		if not $AudioStreamPlayer3D.playing and is_on_floor():
+			$AudioStreamPlayer3D.play()
+		
+		#if not $AnimationPlayer.is_playing():
+		#	$AnimationPlayer.play("camera_bob")
+	
+	
+	
+	elif direction == Vector3.ZERO:
+		$AnimationPlayer.play("RESET")
+		var tween: Tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+		await tween.tween_property($AudioStreamPlayer3D, "volume_db", -6, 0.5).finished
+		$AudioStreamPlayer3D.stop()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity.y -= component_holder.get_component_member("Physics", "gravity") * delta
 	
 	# Handle Jump.
 	if Input.is_action_pressed("move_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = component_holder.get_component_member("Physics", "launch_velocity")
 	
 	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		if Input.is_action_pressed("move_sprint"):
+			var tween: Tween = get_tree().create_tween()
+			tween.tween_property(camera, "fov", 90, .2)
+			$AnimationPlayer.speed_scale = 1.5
+			velocity.x = direction.x * (speed + 3.0)
+			velocity.z = direction.z * (speed + 3.0)
+		else:
+			var tween: Tween = get_tree().create_tween()
+			tween.tween_property(camera, "fov", 75, .1)
+			$AnimationPlayer.speed_scale = 1.0
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-#		velocity.x = 0
-#		velocity.z = 0
+		velocity.x = 0
+		velocity.z = 0
 	
 	move_and_slide()
