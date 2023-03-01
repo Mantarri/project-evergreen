2b46ee7bd710f356e2ffeff0e646310f2baf6ef8
Mistery Man <themisteryman2@outlook.com>
2023-03-01 13:46:46 -0800

    Add: `PhysicsComponent`

--- scenes/entities/components/physics_component.gd
+++ scenes/entities/components/physics_component.gd
@@ -1,12 +1,16 @@
-extends Node
+extends Component
 
 class_name PhysicsComponent
 
-@export var gravity: float = 17.64
+@export var gravity: float = 34.0
 
 # Not sure if I'll use this. But just in case
 @export var inertia: float = 0.0
 
 @export var speed: float = 5.0
 
-@export var launch_velocity: float = 7.75
+@export var launch_velocity: float = 10.5
+
+func _init() -> void:
+	super()
+	id = "Physics"
